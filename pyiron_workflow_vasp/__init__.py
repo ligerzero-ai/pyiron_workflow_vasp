4ef9d44587868b9bab6f50d09a92acd1a0045830
Han Lin Mai <h.mai@mpie.de>
2025-04-18 15:49:37 +0200

    python package

---
+++ pyiron_workflow_vasp/__init__.py
@@ -0,0 +1,8 @@
+"""
+pyiron_workflow_vasp - A VASP workflow integration package for pyiron
+"""
+
+from .vasp import *
+from .generic import *
+
+__version__ = "0.1.0" 
---
+++ pyiron_workflow_vasp/vasp_parser/__init__.py
@@ -0,0 +1,7 @@
+"""
+VASP parser module for pyiron_workflow_vasp
+"""
+
+from .parser import *
+
+__all__ = ["VaspParser"] 
